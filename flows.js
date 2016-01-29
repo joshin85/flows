0da7e1fda84e3601654e43e41fe8378321599a46
Shinjo Melosh <joshin85@gmail.com>
2016-01-28 17:39:39 -0800

    minor functionality added
    Added generic dom update functionality
    *data-{{anticedent}}-{{variable key}} will be targeted for update
    this is temporary functionality that will be updated to use a lower
    impact text parsing method.  For now this will require a parent element
    to be present with the aforementioned data attribute.
    
    default anticedent is = "flows"
    default variable key is = variable.key

--- flows.js
+++ flows.js
@@ -9,8 +9,10 @@ var Observer = new Observer();
 function Observer(){
 	
 	//vars 
+	this.antecedent = "flows"
 	this.notaries;
-	
+/* Functionality is deprecated for now this will act as namespace for project*/
+
 	this.init = function(){
 		this.notaries = [];
 	}
@@ -19,7 +21,7 @@ function Observer(){
 		this.notaries.push(notary);
 	}
 	
-	this.propogateValue = function(key, value) {
+	this.propogateVar = function(key, Var) {
 	
 	}
 	
@@ -31,17 +33,17 @@ function Observer(){
 	}
 	
 	this.init();
-	
+
 }
 
 //Override this prototype to change functionality of the notification method
 function listenerMethods(){
 
-	this.propogateEvent = function(value) {
+	this.propogateEvent = function(Var) {
 		Observer.notify();
 	}
 	
-	this.propogateValue = function(value) {
+	this.propogateVar = function(Var) {
 
 	}
 	
@@ -49,43 +51,45 @@ function listenerMethods(){
 		return this[this.key] ;
 	}
 	
-	this.setListener = function(val) {
-		console.log(val);
-		this[this.key] = val;
+	this.setListener = function(Var) {
+		this[this.key] = Var ;
+		this.Var = Var ;
+		
+		/*This functionality will be replaced by a non dom based parse so that parent elements for variables arent required and the dom is not inflated by to much */
+		//Select ALL dom elements with data attribute of this Variable Key 
+		var domArray = document.querySelectorAll('[data-' + Observer.antecedent + '-' + this.key + ']')
+		domArrayLength = domArray.length;
+		for(var i = 0 ; i < domArrayLength ; i++) {
+			domArray[i].innerText = this[this.key];
+		}
 	}
 }
 
-//Add Super Class
-Value.prototype = new listenerMethods();
-function Value(key, value){
+//Add Prototype
+Variable.prototype = new listenerMethods();
+function Variable(key, Var){
 	
-	//Setter - Propogate events when value is set 
-	this.setValue = function(newValue) {
-		this[thiskey] = newValue;
-	}
-	
-	//Getter
-	this.getValue = function() {
-		return this[this.key];
-	}
-	
-	this.getKey = function(){
-		return this.key; 
-	}
-	
-	this.setKey = function(newKey) {
-		delete this[this.key];
-		this.key = newKey;
-	}
-	
-	this.init = function(key, value){
-		this[key] = value;
+	this.init = function(key, Var){
+		this[key] = Var;
 		this.key = key; 
+		this.Var = Var;
+		/** /
+		//If original object is modified then do callback (OTHERWISE ONLY MODIFYING THROUGH DATA OBJECT WILL DO CALLBACK)
+		Object.defineProperty(this, this.key, {
+			get: function(val) {
+				return this;
+			},
+			set: function(val) {
+				// Add event listeners to newVar: Called when newVar is set 
+				this.setListener(val);
+			}
+		});
+		/**/
 	}	
 	
-	this.init(key, value);
+	this.init(key, Var);
 	this.key;
-	this.value; 
+	this.Var; 
 	
 }
 
@@ -97,26 +101,34 @@ function Data(){
 	}
 	
 	this.dataChange = function() {
-		//When a data value from this DataContainer is changed this event is called...
-		document.getElementById("test").textContent = this.getValue(); 
+		//When a data Var from this DataContainer is changed this event is called...
+		//document.getElementById("test").textContent = this.getVar(); 
 	}
 	
-	this.addData = function(newValue) {
-		this[newValue.key] = newValue;
-		/*-------------------------------------------------------------------------------*/
-		//Add the getter and setter functions that are called when newValue is manipulated
+	this.addData = function(newVar, varName) {
+	//If passed object with superclass of listenerMethods construct then add new variable to Data Object ELSE assume newVar needs to be constructed; 
+		if(typeof newVar != typeof undefined && newVar.constructor.name == "listenerMethods") {
+			this[newVar.key] = newVar;
+		} else if(typeof varName != typeof undefined) {
+			newVar = new Variable(varName, newVar);
+			this[varName] = newVar;
+		} else 
+			console.error("This function requires an input whose prototype is listenerMethods or a value key pair : addData(value, key)");		
+		
+	/*-------------------------------------------------------------------------------*/
+		//Add the getter and setter functions that are called when newVar is manipulated
 		//THIS CODE MAKES THE OBJECT ITSELF IN ACCESSABLE -- You can
-		Object.defineProperty(this, newValue.key, {
+		Object.defineProperty(this, newVar.key, {
 			get: function() {
-				// Add event listeners to newValue: Called when newValue is accessed 
-				return newValue.getListener();
+				// Add event listeners to newVar: Called when newVar is accessed 
+				return newVar.getListener();
 			},
 			set: function(val) {
-				// Add event listeners to newValue: Called when newValue is set 
-				newValue.setListener(val);
+				// Add event listeners to newVar: Called when newVar is set 
+				newVar.setListener(val);
 			}
 		});
-		//Add the getter and setter functions that are called when newValue is manipulated
+		//Add the getter and setter functions that are called when newVar is manipulated
 	/*-------------------------------------------------------------------------------*/
 	}
 	
@@ -124,7 +136,38 @@ function Data(){
 	
 }
 
-//Add Super Class
+domStyle.prototype = new Data(); 
+function domStyle(){
+	/** /
+	//Ints
+	this.top;
+	this.left;
+	this.bottom;
+	this.right;
+	//Strings
+	this.displayType = "";
+	this.class = "";
+	this.style = "";
+	
+	this.generateStyle = function(){
+		
+	}
+	
+	this.init = function(){
+		this.addData("top", "");
+		this.addData("left", "");
+		this.addData("bottom", "");
+		this.addData("right", "");
+		this.addData("displayType", "");
+		this.addData("style", "");
+		this.addData("class", "");
+	}
+	
+	this.init();
+	/**/
+} 
+
+//Add Prototype
 domElement.prototype = new Data();
 function domElement(){
 	
@@ -132,10 +175,17 @@ function domElement(){
 		this.prototype = new Data();
 	}
 	
-	this.newValue = function(newValue) {
-		this.addData(newValue);
-	
+	this.newVar = function(newVar, varName) {
+		this.addData(newVar, varName);
 	}
+		
+	this.domAnalyzer = function(target) {
+		
+	}
+	
+	this.domStyle = new domStyle();
+	this.currentStage;
+	this.stageList;
 
 	this.init();
 }
